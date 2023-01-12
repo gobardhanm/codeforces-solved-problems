aa67507274e374b6a1ae754fc061ce52b26f1203
gobardhanm <gobardhanms364@gmail.com>
2023-01-12 16:32:26 +0530

    Number Transformation

---
+++ Codeforces-Round#786-D3/a.cpp
@@ -0,0 +1,42 @@
+//Gobardhan Meher
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+#define ll long long
+#define nline "\n"
+#define fori for(int i= 0; i<n; ++i)
+#define forj for(int j = 0; j< n; ++j)
+#define pb push_back
+#define pp pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+
+//Problem: Number Transformation
+
+void solve() {
+    int x, y;
+    cin >> x >> y;
+    if(y % x != 0){
+        cout << 0 << " " << 0 << nline;
+    }else{
+        cout << 1 << " " << y/x << nline;
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int tc = 1;
+    cin >> tc;
+    for(int i = 1; i <= tc; i++) {
+        solve();
+    }
+    
+    return 0;
+}
