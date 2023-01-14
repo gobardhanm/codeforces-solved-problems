b91dd4c7acc6d0bab6e6e64b02c5d7411d6cd8b2
gobardhanm <gobardhanms364@gmail.com>
2023-01-14 21:26:33 +0530

    Vlad and Candies

---
+++ Codeforces-Round#780-D3/b.cpp
@@ -0,0 +1,49 @@
+//Gobardhan Meher
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+#define ll long long
+#define nline "\n"
+#define fori for(int i= 0; i<n; ++i)
+#define forj for(int j = 0; j< n; ++j)
+#define pb push_back
+#define pp pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+
+//Problem: Vlad and Candies
+
+void solve() {
+    int n; cin >> n;
+    int a[n];
+    for(int i = 0; i < n; i++){
+        cin >> a[i];
+    }
+    sort(a, a+n);
+    if(n == 1){
+        if(a[0] > 1) cout << "NO" << nline;
+        else cout << "YES" << nline;
+    }else if((a[n - 1]) > a[n -2] + 1){
+        cout << "NO" << nline;
+    }else{
+        cout << "YES" << nline;
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int tc = 1;
+    cin >> tc;
+    for(int i = 1; i <= tc; i++) {
+        solve();
+    }
+    
+    return 0;
+}
